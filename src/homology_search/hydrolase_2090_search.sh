8cbbaa6a1ae3b188b7858c23053face6a299f0ed
Romain Strock <romain.strock@gmail.com>
2024-09-29 15:21:53 +0100

    Hydrolase search outputs

--- src/homology_search/hydrolase_2090_search.sh
+++ src/homology_search/hydrolase_2090_search.sh
@@ -9,44 +9,37 @@ start=$(date +%s)
 
 cd $HOME
 mkdir -p chahrazad
-export SENSITIVITY="7.5"
 
-# First pass
 INPUT="${HOME}/chahrazad/2090.fa"
 GTDB_OUTPUT="chahrazad/2090_search_gtdb"
 UNIPROT_OUTPUT="chahrazad/2090_search_uniprot"
-
-# Second pass
-# INPUT="${HOME}/chahrazad/2090_homologs_first_pass.fasta"
-# GTDB_OUTPUT = "chahrazad/2090_search_gtdb_expanded"
-# UNIPROT_OUTPUT = "chahrazad/2090_search_uniprot_expanded"
+export SENSITIVITY="7.5"
 
 mkdir -p $GTDB_OUTPUT
 mkdir -p $UNIPROT_OUTPUT
 
+echo "Search in GTDB"
 export INPUT_FASTA=$INPUT
 export TARGET_DB="GTDB_214"
 export OUTPUT_FOLDER=$GTDB_OUTPUT
 bash mmseqs_db_search.sh
 
-export INPUT_FASTA="${GTDB_OUTPUT}/results.fasta"
-export TARGET_DB="db_proka"
-export OUTPUT_TSV="${GTDB_OUTPUT}/results_gtdb_mapped_to_db_proka.tsv"
-bash map_sequences_to_db.sh
-
+echo "Search in UniProtKB"
 export INPUT_FASTA=$INPUT
 export TARGET_DB="UniProtKB"
 export OUTPUT_FOLDER=$UNIPROT_OUTPUT
 bash mmseqs_db_search.sh
 
-export INPUT_FASTA="${UNIPROT_OUTPUT}/results.fasta"
-export TARGET_DB="db_proka"
-export OUTPUT_TSV="${UNIPROT_OUTPUT}/results_uniprot_mapped_to_db_proka.tsv"
-bash map_sequences_to_db.sh
-
+echo "Map GTDB results to UniProtKB"
 export INPUT_FASTA="${GTDB_OUTPUT}/results.fasta"
 export TARGET_DB="UniProtKB"
 export OUTPUT_TSV="${UNIPROT_OUTPUT}/results_uniprot_mapped_to_gtdb.tsv"
+bash map_sequences_to_db.sh
+
+echo "Map GTDB results to our 'db_proka' database (Strock et al., 2024 - DOI: 10.1101/2024.09.18.613068)"
+export INPUT_FASTA="${GTDB_OUTPUT}/results.fasta"
+export TARGET_DB="db_proka"
+export OUTPUT_TSV="${GTDB_OUTPUT}/results_gtdb_mapped_to_db_proka.tsv"
 bash map_sequences_to_db.sh
 
 end=$(date +%s)
